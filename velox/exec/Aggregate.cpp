87534ed50a25ac15b1d42df3c5fb619ced8bd154
Hongze Zhang <hongze.zhang@intel.com>
2023-12-29 10:31:07 +0800

    [oap   ] Register merge extract companion agg functions without suffix
    And reverts #14934
    
    Alchemy-item: (ID = 674) [oap   ] Register merge extract companion agg functions without suffix commit 1/2 - c8c8ea6bad7b040f099a3978d8f280147749f354

--- velox/exec/Aggregate.cpp
+++ velox/exec/Aggregate.cpp
@@ -20,6 +20,7 @@
 #include "velox/exec/AggregateCompanionAdapter.h"
 #include "velox/exec/AggregateCompanionSignatures.h"
 #include "velox/exec/AggregateWindow.h"
+#include "velox/expression/SignatureBinder.h"
 
 namespace facebook::velox::exec {
 
