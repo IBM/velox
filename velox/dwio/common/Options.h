895f95705155352cb63e4892aca5ea250bfd560a
Rui Mo <rui@apache.org>
2025-06-04 11:32:32 +0100

    13620 fix: Add config for requested type check in ReaderBase::convertType
    Signed-off-by: Yuan <yuanzhou@apache.org>
    
    Alchemy-item: (ID = 505) 13620 fix: Add config for requested type check in ReaderBase::convertType commit 1/1 - b5541ba555c0cc39c801f1a916191a52377c2735

--- velox/dwio/common/Options.h
+++ velox/dwio/common/Options.h
@@ -639,6 +639,10 @@ class ReaderOptions : public io::ReaderOptions {
     return randomSkip_;
   }
 
+  bool enableRequestedTypeCheck() const {
+    return enableRequestedTypeCheck_;
+  }
+
   void setRandomSkip(std::shared_ptr<random::RandomSkipTracker> randomSkip) {
     randomSkip_ = std::move(randomSkip);
   }
@@ -675,6 +679,10 @@ class ReaderOptions : public io::ReaderOptions {
     allowEmptyFile_ = value;
   }
 
+  void setEnableRequestedTypeCheck(bool enableRequestedTypeCheck) {
+    enableRequestedTypeCheck_ = enableRequestedTypeCheck;
+  }
+
  private:
   uint64_t tailLocation_;
   FileFormat fileFormat_;
@@ -692,6 +700,7 @@ class ReaderOptions : public io::ReaderOptions {
   bool adjustTimestampToTimezone_{false};
   bool selectiveNimbleReaderEnabled_{false};
   bool allowEmptyFile_{false};
+  bool enableRequestedTypeCheck_{true};
 };
 
 struct WriterOptions {
