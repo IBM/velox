52256c5713f7b2d82437f282a3aa8046aa11b430
Minhan Cao <mcao@ibm.com>
2025-11-05 20:41:38 -0800

    Report number of lookups to the AsyncDataCache
    Add runtime stats for number of lookups to the AsyncDataCache.
    
    Alchemy-item: (ID = 1442) Report number of lookups to the AsyncDataCache commit 1/1 - 840f2a4a3e2f4efa0e3b76538ae03116f03a7729

--- velox/common/caching/AsyncDataCache.cpp
+++ velox/common/caching/AsyncDataCache.cpp
@@ -253,6 +253,7 @@ std::optional<CachePin> CacheShard::lookupLocked(
     uint64_t size,
     folly::SemiFuture<bool>* wait) {
   ++eventCounter_;
+  ++numLookup_;
   auto it = entryMap_.find(key);
   if (it == entryMap_.end()) {
     return std::nullopt;
@@ -681,6 +682,7 @@ void CacheShard::updateStats(CacheStats& stats) {
     ++stats.numEntries;
     entry->updateDataStats(stats);
   }
+  stats.numLookup += numLookup_;
   stats.numHit += numHit_;
   stats.hitBytes += hitBytes_;
   stats.numNew += numNew_;
@@ -777,6 +779,7 @@ bool CacheShard::removeFileEntries(
 
 CacheStats CacheStats::operator-(const CacheStats& other) const {
   CacheStats result;
+  result.numLookup = numLookup - other.numLookup;
   result.numHit = numHit - other.numHit;
   result.hitBytes = hitBytes - other.hitBytes;
   result.numNew = numNew - other.numNew;
