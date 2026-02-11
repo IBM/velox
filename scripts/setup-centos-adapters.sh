35c54a2902db60de21cc5eeeab4cf20db04a7d3d
Luis Garcés-Erice <lga@zurich.ibm.com>
2026-02-11 13:21:30 +0000

    Include nvrtc library

--- scripts/setup-centos-adapters.sh
+++ scripts/setup-centos-adapters.sh
@@ -122,7 +122,8 @@ function install_cuda_runtime {
   dnf install -y \
     cuda-cudart-"$dashed" \
     cuda-compat-"$dashed" \
-    libcufile-"$dashed"
+    libcufile-"$dashed" \
+    cuda-nvrtc-"$dashed"
 }
 
 function install_adapters_deps_from_dnf {
