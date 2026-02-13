f7fde402e205fc9980a3bd6832488afe2dc5a611
Ping Liu <ping.liu.ping@gmail.com>
2026-02-13 10:34:31 +0000

    Iceberg core code
    Co-authored-by: Li Zhou <unidevel@hotmail.com>
    
    Alchemy-item: (ID = 1153) Iceberg staging hub commit 1/6 - c5a69de3d1021073c13a99e1c7c6d6fcce355178
    
    refactor: Move toValues from InPredicate.cpp to Filter.h
    
    The function toValues removes duplicated values from the vector and
    return them in a std::vector. It was used to build an InPredicate. It
    will be needed for building NOT IN filters for Iceberg equality delete
    read as well, therefore moving it from velox/functions/prestosql/InPred
    icate.cpp to velox/type/Filter.h. This commit also renames it to
    deDuplicateValues to make it easier to understand.
    
    feat(connector): Support reading Iceberg split with equality deletes
    
    This commit introduces EqualityDeleteFileReader, which is used to read
    Iceberg splits with equality delete files. The equality delete files
    are read to construct domain filters or filter functions, which then
    would be evaluated in the base file readers.
    
    When there is only one equality delete field, and when that field is
    an Iceberg identifier field, i.e. non-floating point primitive types,
    the values would be converted to a list as a NOT IN domain filter,
    with the NULL treated separately. This domain filter would then be
    pushed to the ColumnReaders to filter our unwanted rows before they
    are read into Velox vectors. When the equality delete column is a
    nested column, e.g. a sub-column in a struct, or the key in a map,
    such column may not be in the base file ScanSpec. We need to add/remove
    these subfields to/from the SchemaWithId and ScanSpec recursively if
    they were not in the ScanSpec already. A test is also added for such
    case.
    
    If there are more than one equality delete field, or the field is not
    an Iceberg identifier field, the values would be converted to a typed
    expression in the conjunct of disconjunts form. This expression would
    be evaluated as the remaining filter function after the rows are read
    into the Velox vectors. Note that this only works for Presto now as
    the "neq" function is not registered by Spark. See https://github.com/
    facebookincubator/velox/issues/12667
    
    Note that this commit only supports integral types. VARCHAR and
    VARBINARY need to be supported in future commits (see
    https://github.com/facebookincubator/velox/issues/12664).
    
    Co-authored-by: Naveen Kumar Mahadevuni <Naveen.Mahadevuni@ibm.com>
    
    Alchemy-item: (ID = 1153) Iceberg staging hub commit 2/6 - 14edb98c67f1c572a5f40682923795bd5b08e7c3
    
    Support insert data into iceberg table.
    
    Add iceberg partition transforms.
    
    Co-authored-by: Chengcheng Jin <Chengcheng.Jin@ibm.com>
    
    Add NaN statistics to parquet writer.
    
    Collect Iceberg data file statistics in dwio.
    
    Integrate Iceberg data file statistics and adding unit test.
    
    Support write field_id to parquet metadata SchemaElement.
    
    Implement iceberg sort order
    
    Add clustered Iceberg writer mode.
    
    Fix parquet writer ut
    
    Add IcebergConnector
    
    Fix unittest error
    
    Resolve confict
    
    Resolve confict
    
    Fix test build issue
    
    Fix crash
    
    Alchemy-item: (ID = 1271) Iceberg core code commit 1/1 - 0e0bf9463a36e206d3f33520564938197f211b43

--- velox/connectors/hive/HiveDataSink.h
+++ velox/connectors/hive/HiveDataSink.h
@@ -18,7 +18,6 @@
 #include "velox/common/compression/Compression.h"
 #include "velox/connectors/Connector.h"
 #include "velox/connectors/hive/HiveConfig.h"
-#include "velox/connectors/hive/HivePartitionName.h"
 #include "velox/connectors/hive/PartitionIdGenerator.h"
 #include "velox/connectors/hive/TableHandle.h"
 #include "velox/dwio/common/Options.h"
@@ -256,7 +255,37 @@ class HiveInsertTableHandle : public ConnectorInsertTableHandle {
       std::shared_ptr<const FileNameGenerator> fileNameGenerator =
           std::make_shared<const HiveInsertFileNameGenerator>(),
       const std::unordered_map<std::string, std::string>& storageParameters =
-          {});
+          {})
+      : inputColumns_(std::move(inputColumns)),
+        locationHandle_(std::move(locationHandle)),
+        storageFormat_(storageFormat),
+        bucketProperty_(std::move(bucketProperty)),
+        compressionKind_(compressionKind),
+        serdeParameters_(serdeParameters),
+        writerOptions_(writerOptions),
+        ensureFiles_(ensureFiles),
+        fileNameGenerator_(std::move(fileNameGenerator)) {
+    if (compressionKind.has_value()) {
+      VELOX_CHECK(
+          compressionKind.value() != common::CompressionKind_MAX,
+          "Unsupported compression type: CompressionKind_MAX");
+    }
+
+    if (ensureFiles_) {
+      // If ensureFiles is set and either the bucketProperty is set or some
+      // partition keys are in the data, there is not a 1:1 mapping from Task to
+      // files so we can't proactively create writers.
+      VELOX_CHECK(
+          bucketProperty_ == nullptr || bucketProperty_->bucketCount() == 0,
+          "ensureFiles is not supported with bucketing");
+
+      for (const auto& inputColumn : inputColumns_) {
+        VELOX_CHECK(
+            !inputColumn->isPartitionKey(),
+            "ensureFiles is not supported with partition keys in the data");
+      }
+    }
+  }
 
   virtual ~HiveInsertTableHandle() = default;
 
@@ -766,7 +795,7 @@ class HiveDataSink : public DataSink {
   // to each corresponding (bucketed) partition based on the partition and
   // bucket ids calculated by 'computePartitionAndBucketIds'. The function also
   // ensures that there is a writer created for each (bucketed) partition.
-  void splitInputRowsAndEnsureWriters();
+  virtual void splitInputRowsAndEnsureWriters(RowVectorPtr input);
 
   // Makes sure to create one writer for the given writer id. The function
   // returns the corresponding index in 'writers_'.
@@ -780,6 +809,9 @@ class HiveDataSink : public DataSink {
   std::unique_ptr<facebook::velox::dwio::common::Writer> createWriterForIndex(
       size_t writerIndex);
 
+  virtual std::optional<std::string> getPartitionName(
+      const HiveWriterId& id) const;
+
   // Creates and configures WriterOptions based on file format.
   // Sets up compression, schema, and other writer configuration based on the
   // insert table handle and connector settings.
@@ -790,14 +822,8 @@ class HiveDataSink : public DataSink {
   // Creates WriterOptions for a specific writer index. Use this overload
   // during writer rotation to ensure the correct writer's memory pool and
   // nonReclaimableSection are used.
-  std::shared_ptr<dwio::common::WriterOptions> createWriterOptions(
+  virtual std::shared_ptr<dwio::common::WriterOptions> createWriterOptions(
       size_t writerIndex) const;
-
-  // Returns the Hive partition directory name for the given partition ID.
-  // Converts the partition values associated with the partition ID into a
-  // Hive-formatted directory path. Returns std::nullopt if the table is
-  // unpartitioned. Should be called only when writing to a partitioned table.
-  virtual std::string getPartitionName(uint32_t partitionId) const;
 
   std::unique_ptr<facebook::velox::dwio::common::Writer>
   maybeCreateBucketSortWriter(
@@ -811,6 +837,8 @@ class HiveDataSink : public DataSink {
   // accommodate all rows.
   void
   updatePartitionRows(uint32_t index, vector_size_t numRows, vector_size_t row);
+
+  void extendBuffersForPartitionedTables();
 
   HiveWriterParameters getWriterParameters(
       const std::optional<std::string>& partition,
@@ -838,14 +866,14 @@ class HiveDataSink : public DataSink {
   /// Rotates the writer at the given index to a new file. This is called when
   /// the current file exceeds maxTargetFileBytes_. The old writer is closed
   /// and a new writer is created for the same partition/bucket.
-  void rotateWriter(size_t index);
+  virtual void rotateWriter(size_t index);
 
   /// Finalizes the current file for the writer at the given index.
   /// Captures file stats and adds the file info to writtenFiles.
   /// Called by rotateWriter() and closeInternal().
   void finalizeWriterFile(size_t index);
 
-  void closeInternal();
+  virtual void closeInternal();
 
   // IMPORTANT NOTE: these are passed to writers as raw pointers. HiveDataSink
   // owns the lifetime of these objects, and therefore must destroy them last.
