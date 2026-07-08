6035b0ae87454e033a09be9db39c043038031ab7
Wechar Yu <yuwq1996@gmail.com>
2026-07-08 23:53:53 +0800

    fix(parquet): Flush row group by buffered bytes in the writer
    Differential Revision: D111035585
    
    Pull Request resolved: https://github.com/facebookincubator/velox/pull/16998

--- velox/dwio/parquet/tests/ParquetTestBase.cpp
+++ velox/dwio/parquet/tests/ParquetTestBase.cpp
@@ -251,19 +251,20 @@ dwio::common::MemorySink* ParquetTestBase::write(
 
 dwio::common::MemorySink* ParquetTestBase::write(
     const std::vector<RowVectorPtr>& batches,
-    const WriterOptions& writerOptions) {
+    const dwio::common::WriterOptions& options,
+    const ParquetWriterOptions& writerOptions) {
   VELOX_CHECK(!batches.empty());
   auto sink = std::make_unique<dwio::common::MemorySink>(
       200 * 1024 * 1024,
       dwio::common::FileSink::Options{.pool = leafPool_.get()});
   auto* sinkPtr = sink.get();
+  auto writerOptionsBase = options;
+  writerOptionsBase.formatSpecificOptions =
+      std::make_shared<ParquetWriterOptions>(writerOptions);
   auto writer = std::make_unique<Writer>(
-      std::move(sink), writerOptions, batches[0]->rowType());
+      std::move(sink), writerOptionsBase, batches[0]->rowType());
   for (size_t i = 0; i < batches.size(); ++i) {
     writer->write(batches[i]);
-    if (i + 1 < batches.size()) {
-      writer->flush();
-    }
   }
   writer->close();
   writers_.push_back(std::move(writer));
